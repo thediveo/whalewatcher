1fae9e069eec6d64108917f23bcb382228bbbdb1
thediveo <thediveo@gmx.eu>
2023-09-12 11:34:03 +0000

    feat: extend containerd namespace ignores due to cri engine client overlap
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- engineclient/containerd/doc.go
+++ engineclient/containerd/doc.go
@@ -5,5 +5,7 @@ Package containerd implements the containerd EngineClient.
 
 containerd's task states of pausing and paused are both mapped to a paused
 container from the perspective of the whalewatcher module.
+
+This engine client by default ignores the "moby" and "k8s.io" namespaces.
 */
 package containerd
